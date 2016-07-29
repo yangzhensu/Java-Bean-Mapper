56bc43ab43865c84e670c429ea5d3562cc88696d
Zhensu Yang <yangzhensu@gmail.com>
2016-07-29 16:35:57 +0800

    Generated file

---
+++ TestMapper.java
@@ -0,0 +1,38 @@
+import com.stonybrook.zhensuyang.javabeanmapper.domain.SourceClass;
+import com.stonybrook.zhensuyang.javabeanmapper.domain.TargetClass;
+
+public class TestMapper {
+	public TargetClass map(SourceClass source) {
+		TargetClass target = new TargetClass();
+		target.setName(source.getName());
+		target.setAge(source.getAge());
+		target.setAddress(source.getAddress());
+		target.setTargetA(source.getSourceA());
+		target.setTargetB(source.getSourceB());
+		target.setTargetC(source.getSourceC());
+		target.setListA(source.getListA());
+		return target;
+	}
+
+	private List<TargetType> mapListCToListC(List<SourceType> list) {
+		List<TargetType> res = new ArrayList<>();
+		for (SourceType src : list) {
+			TargetType tgt = new TargetType();
+			tgt.setTypeA(src.getTypeA);
+			tgt.setTypeB(src.getTypeB);
+			res.add(tgt);
+		}
+		return res;
+	}
+
+	private List<TargetType> mapListBToListB(List<SourceType> list) {
+		List<TargetType> res = new ArrayList<>();
+		for (SourceType src : list) {
+			TargetType tgt = new TargetType();
+			tgt.setTypeA(src.getTypeA);
+			tgt.setTypeB(src.getTypeB);
+			res.add(tgt);
+		}
+		return res;
+	}
+}
